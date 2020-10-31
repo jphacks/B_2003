9e8fd8b864ed25ce0b939ecc6ef03d7879a1b70d
shunya-s15 <shunya.suraimu@gmail.com>
2020-10-31 20:24:17 +0900

    a

--- front/pages/group_registration.js
+++ front/pages/group_registration.js
@@ -1,17 +1,17 @@
 import Link from 'next/link'
-//import './group.css'
 
+//import './group.css'
 
 
 export default () =><div>
     <h1>Group registration</h1>
     <div><h1>"サービス名"へようこそ</h1>登録情報を入力してください</div>
-    <form onSubmit='/group_log_in.js'>
-        <p>団体名:<input type="text" name="name"required></input></p>
+    <form>
+        <p>団体名:<input type="text" name="name" required></input></p>
         <p>メールアドレス:<input type="email" name="mailadress" required></input></p>
         <p>パスワード:<input type="text" name="password" minLength="5" required></input></p>
         <p>同時に活動できる人数:<input type="number" name="limit" required></input></p>
-        <input type="submit" value="新規登録"></input>
+        <input type="submit" value="新規登録" onClick></input>
     </form>
     <div>
         <Link href = "/group_log_in">
