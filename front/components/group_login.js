5036d6d8756b1a863062df2f722ba8aa0c627f54
kkota <kota111709@outlook.jp>
2020-10-31 21:00:20 +0900

    grouploginのcomponent化

---
+++ front/components/group_login.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Link from 'next/link'
+
+export default class Grouplogin extends Component {
+
+    constructor(props) {
+        super(props);
+        this.sendinfo = this.sendinfo.bind(this);
+    }
+
+    sendinfo(){
+
+    }
+
+    render(){
+        return (<div>
+        <h1>Group Log in</h1>
+        <div>ログインに必要な情報を入力してください</div>
+        <form onSubmit='/log_in'>
+            <p>メールアドレス:<input type="email" name="email" required></input></p>
+            <p>パスワード:<input type="text" name="password" required></input></p>
+            <input type="submit" value="ログイン"></input>
+        </form>
+        <div>
+            <Link href = "/group_registration">
+                <a>新規登録</a>
+            </Link>
+        </div>
+        <div>
+            <Link href = "/log_in">
+                <a>顔認証</a>
+            </Link>
+        </div>
+        <div>
+            <Link href = "/">
+                <a>Go home</a>
+            </Link>
+        </div>
+    </div>)
+    }
+}
--- front/pages/group_log_in.js
+++ front/pages/group_log_in.js
@@ -1,26 +1,6 @@
 import Link from 'next/link'
+import Grouplogin from '../components/group_login'
 
 export default () =><div>
-    <h1>Group Log in</h1>
-    <div>ログインに必要な情報を入力してください</div>
-    <form onSubmit='/log_in'>
-        <p>メールアドレス:<input type="email" name="email" required></input></p>
-        <p>パスワード:<input type="text" name="password" required></input></p>
-        <input type="submit" value="ログイン"></input>
-    </form>
-    <div>
-        <Link href = "/group_registration">
-            <a>新規登録</a>
-        </Link>
-    </div>
-    <div>
-        <Link href = "/log_in">
-            <a>顔認証</a>
-        </Link>
-    </div>
-    <div>
-        <Link href = "/">
-            <a>Go home</a>
-        </Link>
-    </div>
+    <Grouplogin/>
 </div>
