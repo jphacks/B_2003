2e526b1d900e3203ea1cd4a00f8f10e240f83896
shunya-s15 <shunya.suraimu@gmail.com>
2020-11-03 00:19:37 +0900

    できた～

--- front/components/showmember.js
+++ front/components/showmember.js
@@ -4,11 +4,10 @@ import Link from 'next/link'
 class Showmember extends Component {
     constructor(props){
         super(props);
-        this.showmember = this.showmember.bind(this);
     }
 
-    showmember(){
-        const members = new Array('shunya','kotaro','naoya','kohei');
+    componentDidMount(){
+        const members = new Array('shunya','kotaro','naoya','kohei','uec');
         const timeline = document.getElementById("member");
         timeline.innerHTML = "";
         for(var i=0;i<members.length;i++){
@@ -23,7 +22,6 @@ class Showmember extends Component {
         <div>
             <h1>活動メンバー</h1>
             <div id = "member"></div>
-            <button onClick = {this.showmember}>メンバー一覧</button>
         </div>)
     }
 }
